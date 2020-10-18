1486f37ac31aa6f2a40a90ba85485f3d6cab4613
gregorybork <gregorybork1@gmail.com>
2020-10-18 14:59:22 -0700

    Updates

---
+++ precache-manifest.1eb6d5dd6c5f7db35cf589403ff06550.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0938929528c9b324cd51cc442d98212",
+    "url": "/react_todo_list/index.html"
+  },
+  {
+    "revision": "203d575e1466a9333ebd",
+    "url": "/react_todo_list/static/css/main.eb7bed82.chunk.css"
+  },
+  {
+    "revision": "e3fb6ce895492cdc0e7b",
+    "url": "/react_todo_list/static/js/2.c61f7a42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo_list/static/js/2.c61f7a42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "203d575e1466a9333ebd",
+    "url": "/react_todo_list/static/js/main.bf2dc43d.chunk.js"
+  },
+  {
+    "revision": "7a46301d3f9b581c9e4e",
+    "url": "/react_todo_list/static/js/runtime-main.217762e6.js"
+  },
+  {
+    "revision": "e93a63998650d7ea771afad90ca895b6",
+    "url": "/react_todo_list/static/media/mainImg.e93a6399.jpg"
+  }
+]);
